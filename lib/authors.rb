2ee83b713599d117af0147cb3b78722479172d08
Catherine Tran & Marina Neumann <student@epicodus.com>
2014-08-18 11:25:33 -0700

    Add save method and compares two occurrences with the same attributes

--- lib/authors.rb
+++ lib/authors.rb
@@ -8,5 +8,19 @@ class Author
 
   def self.all
     @authors = []
+    results = DB.exec("SELECT * FROM authors;")
+    results.each do |result|
+      @authors << Author.new({:name => result['name']})
+    end
+    @authors
   end
+
+  def save
+    results = DB.exec("INSERT INTO authors (name) VALUES ('#{@name}');")
+  end
+
+  def ==(another_author)
+    self.name == another_author.name
+  end
+
 end
--- spec/authors_spec.rb
+++ spec/authors_spec.rb
@@ -11,4 +11,16 @@ describe Author do
     expect(Author.all).to eq []
   end
 
+  it "adds a author and saves it" do
+    test_author = Author.new({:name => 'Tolkien'})
+    test_author.save
+    expect(Author.all).to eq [test_author]
+  end
+
+  it "returns true if two authors have the same attributes" do
+    test_author1 = Author.new({:name => 'Tolkien'})
+    test_author2 = Author.new({:name => 'Tolkien'})
+    expect(test_author1).to eq test_author2
+  end
+
 end
