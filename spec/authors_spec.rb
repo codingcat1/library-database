7868a979b2bef6d039b3efd7def1ef2c03cbfa46
Catherine Tran & Marina Neumann <student@epicodus.com>
2014-08-18 11:16:58 -0700

    create initialize method for author name

--- spec/authors_spec.rb
+++ spec/authors_spec.rb
@@ -1,1 +1,9 @@
 require 'spec_helper'
+
+describe Author do
+  it "initializes a new author with a name" do
+    test_author = Author.new({:name => 'Tolkien'})
+    expect(test_author).to be_an_instance_of Author
+    expect(test_author.name).to eq 'Tolkien'
+  end
+end
